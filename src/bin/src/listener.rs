5b49fece671ef4d11166cead03700d66695acc9b
Ibis Sunrosa <drsunrosa@gmail.com>
2026-06-26 22:08:41 +0000

    like that i guess

--- src/bin/src/listener.rs
+++ src/bin/src/listener.rs
@@ -4,20 +4,19 @@ use kanal::Sender;
 use rdev::{Event, EventType, listen};
 
 pub fn listener_thread(event_sender: Sender<Event>, enable_mouse_motion: bool) {
-  let closure = if enable_mouse_motion {
-    move |event: Event| {
+  let listening_result = if enable_mouse_motion {
+    listen(move |event: Event| {
       event_sender.send(event).unwrap();
-    }
+    })
   } else {
-    move |event: Event| {
+    listen(move |event: Event| {
       if !matches!(event.event_type, EventType::MouseMove { .. }) {
         event_sender.send(event).unwrap();
       }
-    }
+    })
   };
 
-  // This will block the thread.
-  if let Err(error) = listen(closure) {
+  if let Err(error) = listening_result {
     error!("error listening to system events: {:?}", error);
     panic!("error listening to system events: {:?}", error);
   }
